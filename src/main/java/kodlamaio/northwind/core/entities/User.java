a6ef15952e72838515784f89509eb71e9cb51709
DESKTOP-0G02NO8\mustafakaplan <mstfakapln@gmail.com>
2021-05-31 00:31:33 +0300

    user operations

---
+++ src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -0,0 +1,31 @@
+package kodlamaio.northwind.core.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Email
+    @NotBlank
+    @NotNull
+    private String email;
+
+    @NotBlank
+    @NotNull
+    private String passsword;
+}
